3f0d2495204ce939aa015bea35b9a87cab930c4e
dev-rome <info.devrome@gmail.com>
2024-10-11 13:53:25 -0400

    update RootLayout to include ThemeProvider and ThemeToggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat, Roboto } from "next/font/google";
+import { ThemeProvider } from "../context/ThemeContext";
+import ThemeToggle from "../components/ThemeToggle";
 import "./globals.css";
 
 export const montserrat = Montserrat({
@@ -14,13 +16,11 @@ export const roboto = Roboto({
   variable: "--font-roboto",
 });
 
-
 export const metadata: Metadata = {
   title: "Jerome Haynes | Full-Stack Developer & Software Engineer",
   description:
     "Hi there! I'm a software engineer with a deep passion for coding and using technology to make a positive impact. With strong expertise in frontend development, I love creating user-friendly, visually appealing web experiences. While my focus is on the frontend, I also have the skills to build and maintain reliable backend systems. Dive into my projects to see how I leverage technology to solve real-world problems and bring ideas to life.",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,9 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable} ${roboto.variable}`}>
       <body className="antialiased">
-        <main>
-          {children}
-        </main>
+        <ThemeProvider>
+          <ThemeToggle />
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
