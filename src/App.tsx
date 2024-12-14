26376d6815d868ebc65c958c2ac9ce48dd6d9b01
dev-rome <info.devrome@gmail.com>
2024-12-14 12:49:51 -0500

    import layout component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function App() {
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </Layout>
+
+  )
+}
