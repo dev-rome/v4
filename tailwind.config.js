256e593cfe341ebdfa538d1ff4033554c5cf49ac
dev-rome <info.devrome@gmail.com>
2024-12-15 17:37:44 -0500

    Update tailwind.config.js
    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#f8f9fa',
+        dark: '#212529',
+        'text-light': '#212529',
+        'text-dark': '#f8f9fa',
+      },
+    },
   },
   plugins: [],
 }
