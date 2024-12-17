604b197aa37a6494be81b3f1c6f1f00db45a479b
dev-rome <info.devrome@gmail.com>
2024-12-17 14:46:34 -0500

    Update Navigation.tsx
    update navigation component to use nav link component and light and dark toggle button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,49 @@
-import React from "react"
+import React from "react";
+import NavLinks from "./NavLinks";
+import { useTheme } from "../context/ThemeContext";
+import { Link } from "react-router-dom";
+
+const links = [
+  { link: "/about", text: "About" },
+  { link: "/work", text: "Work" },
+  { link: "/contact", text: "Contact" },
+];
 
 export default function Navigation() {
-    return (
-        <div>
-            <h1>
-                hello
-            </h1>
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <nav className="flex items-center justify-between px-8 py-4">
+      <Link
+        to="/"
+        className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 dark:bg-gray-800"
+      >
+        <p className="text-black dark:text-white">DR</p>
+      </Link>
+      <div className="flex gap-4">
+        <ul className="flex space-x-4">
+          {links.map(({ link, text }) => (
+            <li key={text}>
+              <NavLinks link={link} text={text} />
+            </li>
+          ))}
+        </ul>
+        <div className="flex items-center space-x-2">
+          <span className="text-xs text-black dark:text-white">LIGHT</span>
+          <button
+            onClick={toggleTheme}
+            className={`relative inline-flex h-6 w-16 items-center rounded-full transition-colors focus:outline-none ${
+              theme === "light" ? "bg-gray-300" : "bg-gray-600"
+            }`}
+          >
+            <span
+              className={`inline-block h-6 w-6 transform rounded-full bg-white transition-transform ${
+                theme === "light" ? "translate-x-1" : "translate-x-9"
+              }`}
+            />
+          </button>
+          <span className="text-xs">DARK</span>
         </div>
-    )
+      </div>
+    </nav>
+  );
 }
