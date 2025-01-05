bcfd72339ef100ffbb5929bd1df3518deaede7a3
dev-rome <info.devrome@gmail.com>
2025-01-05 15:56:46 -0500

    created corePrinciplesData.ts
    created corePrinciplesData file to hold core principle data

---
+++ src/data/corePrinciplesData.ts
@@ -0,0 +1,28 @@
+const corePrinciplesData = [
+  {
+    number: "01",
+    title: "Make It",
+    description:
+      "I focus on bringing designs to life directly in the browser, creating interactive and responsive experiences that teams and clients can rally around. While design tools provide a great starting point, the true value lies in transforming ideas into tangible, functional links that showcase the potential of a project in real-time.",
+  },
+  {
+    number: "02",
+    title: "Collaborate",
+    description:
+      "Collaboration is at the heart of every successful project. I work closely with teams and clients to bring ideas to life, ensuring clear communication and alignment throughout the process. I focus on creating functional solutions that align with a shared vision. This approach fosters better feedback, quicker iterations, and results that truly resonate with everyone involved.",
+  },
+  {
+    number: "03",
+    title: "Accessible",
+    description:
+      "I prioritize building inclusive and user-friendly experiences that cater to people of all abilities. By following accessibility standards and best practices, I ensure solutions are meaningful, equitable, and easy to use for everyone.",
+  },
+  {
+    number: "04",
+    title: "Keep learning",
+    description:
+      "I believe continuous learning is key to staying ahead in a constantly evolving industry. Whether it's exploring new technologies, refining existing skills, or adopting innovative practices, I stay curious and committed to growth. This dedication ensures I can consistently deliver the best solutions and adapt to new challenges effectively.",
+  },
+];
+
+export default corePrinciplesData;
