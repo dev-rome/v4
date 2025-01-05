38887aa21b81d3cdcdc97c623f2c74b83d4a6bdb
dev-rome <info.devrome@gmail.com>
2025-01-05 16:10:02 -0500

    Update projectData.ts
    edit projectData descriptions

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -13,7 +13,7 @@ const projectData = [
     id: "1",
     title: "Timber and Hatchet",
     description:
-      "Timber & Hatchet is a company that leverages Astro, React, and Tailwind CSS to hire nearshore engineers who assist businesses in achieving their objectives through software enhancements.",
+      "A company leveraging Astro, React, and Tailwind CSS to hire nearshore engineers.",
     image: TimberAndHatchet,
     url: "https://astro-website-v1.netlify.app/",
     githubLink: "https://github.com/dev-rome/Website-Project-v1",
@@ -22,7 +22,7 @@ const projectData = [
     id: "2",
     title: "HBO-Stream",
     description:
-      "HBO-Stream is a Next.js web app built with Tailwind CSS and TypeScript, using The Movie Database (TMDb) API to browse and search for popular movies and TV shows conveniently.",
+      "A Next.js web app using TMDb API to browse and search for popular movies and TV shows.",
     image: HBOStream,
     url: "https://aesthetic-alpaca-95ce00.netlify.app/",
     githubLink: "https://github.com/dev-rome/HBO-Stream",
@@ -31,7 +31,7 @@ const projectData = [
     id: "3",
     title: "Classroom-Connect",
     description:
-      "A virtual classroom platform that allows students to engage with classmates and teachers, fostering meaningful friendships. Built with React, JavaScript, Tailwind CSS, MongoDB, Express, Node.js, and VideoSDK.",
+      "A virtual classroom platform fostering meaningful friendships among students.",
     image: ClassroomConnect,
     url: "https://classroomconnect-frontend.onrender.com/",
     githubLink: "https://github.com/dev-rome/classroom-connect-mern/tree/main",
@@ -40,7 +40,7 @@ const projectData = [
     id: "4",
     title: "Word Game",
     description:
-      "Word Game is a clone of the popular Wordle game, offering players a fun and challenging word puzzle experience where they guess a hidden word within a limited number of attempts.",
+      "A clone of the popular Wordle game, offering a fun and challenging word puzzle experience.",
     image: WordGame,
     url: "https://joyful-capybara-6b00e8.netlify.app/",
     githubLink: "https://github.com/dev-rome/project-wordle",
@@ -49,7 +49,7 @@ const projectData = [
     id: "5",
     title: "Toast Message",
     description:
-      "A toast message is a popular UI element designed to provide brief and unobtrusive notifications to users. It appears on the screen for a short period, resembling the action of toast popping out of a toaster. This functionality ensures that users receive important updates without disrupting their interaction with the application.",
+      "A UI element providing brief and unobtrusive notifications to users.",
     image: ToastMessage,
     url: "https://magnificent-bombolone-d6022d.netlify.app/",
     githubLink: "https://github.com/dev-rome/project-toast",
@@ -58,7 +58,7 @@ const projectData = [
     id: "6",
     title: "Dictionary Web App",
     description:
-      "Dictionary Web App using HTML, CSS, and JavaScript, seamlessly integrating the Free Dictionary API to provide users with quick and reliable word definitions.",
+      "A web app integrating the Free Dictionary API to provide quick and reliable word definitions.",
     image: DictionaryApp,
     url: "https://inquisitive-biscochitos-13dfd2.netlify.app/",
     githubLink: "https://github.com/dev-rome/Dictionary-Web-App",
@@ -67,7 +67,7 @@ const projectData = [
     id: "7",
     title: "Weather App",
     description:
-      "Weather App: Built with Next.js, TypeScript, Tailwind CSS, and the Weatherbit API, providing users with quick and accurate weather information.",
+      "A Next.js app using the Weatherbit API to provide quick and accurate weather information.",
     image: WeatherApp,
     url: "https://example.com",
     githubLink: "https://github.com/dev-rome/Weather-App",
@@ -76,7 +76,7 @@ const projectData = [
     id: "8",
     title: "IP Address Tracker",
     description:
-      "IP Address Tracker is a web application built with React and Tailwind CSS that allows users to track the location of an IP address using the IP Geolocation API.",
+      "A web app that tracks the location of an IP address using the IP Geolocation API.",
     image: IPAddressTracker,
     url: "https://example.com",
     githubLink: "https://github.com/dev-rome/IP-Address-Tracker",
@@ -85,7 +85,7 @@ const projectData = [
     id: "9",
     title: "Clock App",
     description:
-      "Clock App is a simple clock application built with React and Tailwind CSS.",
+      "A simple clock application built with React and Tailwind CSS.",
     image: ClockApp,
     url: "https://example.com",
     githubLink: "https://github.com/dev-rome/Clock-App",
