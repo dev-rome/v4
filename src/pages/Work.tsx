b0ac9bd11483519f86b8ce66458189dd6d35c962
dev-rome <info.devrome@gmail.com>
2025-01-05 15:12:25 -0500

    Update Work.tsx
    update work.tsx to display work section

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import projectData from "../data/projectData";
+import ProjectItem from "../components/ProjectItem";
 
 export default function Work() {
   return (
-    <div>
-      <h2>Work</h2>
-      <p>Welcome to the work page.</p>
-    </div>
+    <section id="work" className="py-12 md:py-20">
+      <div className="mx-auto max-w-7xl">
+        <div className="px-4">
+          <h2 className="text-xl font-bold">WORK</h2>
+          <span className="-mt-2 block h-1 w-20 bg-accent-light dark:bg-accent-dark"></span>
+        </div>
+        <div className="mt-5 grid grid-cols-1 gap-4 px-4 md:grid-cols-2 lg:grid-cols-3">
+          {projectData.map(({id, title, description, image, url, githubLink}) => (
+            <ProjectItem
+              key={id}
+              title={title}
+              description={description}
+              image={image}
+              url={url}
+              githubLink={githubLink}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
