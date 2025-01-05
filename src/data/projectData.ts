8b7bf6a668d329fee4bd73757759fc10b4649171
dev-rome <info.devrome@gmail.com>
2025-01-05 15:04:21 -0500

    Create projectData.ts
    created projectData.ts file to hold data for current and new projects

---
+++ src/data/projectData.ts
@@ -0,0 +1,95 @@
+import TimberAndHatchet from "../assets/timber&hatchet.png";
+import ClassroomConnect from "../assets/classroom-connect.png";
+import HBOStream from "../assets/hbo-stream.png";
+import WordGame from "../assets/word-game.png";
+import ToastMessage from "../assets/toast-message.png";
+import DictionaryApp from "../assets/dictionary-app.png";
+import WeatherApp from "../assets/weather-app.png";
+import IPAddressTracker from "../assets/ip-address-tracker.png";
+import ClockApp from "../assets/clock-app.png";
+
+const projectData = [
+  {
+    id: "1",
+    title: "Timber and Hatchet",
+    description:
+      "Timber & Hatchet is a company that leverages Astro, React, and Tailwind CSS to hire nearshore engineers who assist businesses in achieving their objectives through software enhancements.",
+    image: TimberAndHatchet,
+    url: "https://astro-website-v1.netlify.app/",
+    githubLink: "https://github.com/dev-rome/Website-Project-v1",
+  },
+  {
+    id: "2",
+    title: "HBO-Stream",
+    description:
+      "HBO-Stream is a Next.js web app built with Tailwind CSS and TypeScript, using The Movie Database (TMDb) API to browse and search for popular movies and TV shows conveniently.",
+    image: HBOStream,
+    url: "https://aesthetic-alpaca-95ce00.netlify.app/",
+    githubLink: "https://github.com/dev-rome/HBO-Stream",
+  },
+  {
+    id: "3",
+    title: "Classroom-Connect",
+    description:
+      "A virtual classroom platform that allows students to engage with classmates and teachers, fostering meaningful friendships. Built with React, JavaScript, Tailwind CSS, MongoDB, Express, Node.js, and VideoSDK.",
+    image: ClassroomConnect,
+    url: "https://classroomconnect-frontend.onrender.com/",
+    githubLink: "https://github.com/dev-rome/classroom-connect-mern/tree/main",
+  },
+  {
+    id: "4",
+    title: "Word Game",
+    description:
+      "Word Game is a clone of the popular Wordle game, offering players a fun and challenging word puzzle experience where they guess a hidden word within a limited number of attempts.",
+    image: WordGame,
+    url: "https://joyful-capybara-6b00e8.netlify.app/",
+    githubLink: "https://github.com/dev-rome/project-wordle",
+  },
+  {
+    id: "5",
+    title: "Toast Message",
+    description:
+      "A toast message is a popular UI element designed to provide brief and unobtrusive notifications to users. It appears on the screen for a short period, resembling the action of toast popping out of a toaster. This functionality ensures that users receive important updates without disrupting their interaction with the application.",
+    image: ToastMessage,
+    url: "https://magnificent-bombolone-d6022d.netlify.app/",
+    githubLink: "https://github.com/dev-rome/project-toast",
+  },
+  {
+    id: "6",
+    title: "Dictionary Web App",
+    description:
+      "Dictionary Web App using HTML, CSS, and JavaScript, seamlessly integrating the Free Dictionary API to provide users with quick and reliable word definitions.",
+    image: DictionaryApp,
+    url: "https://inquisitive-biscochitos-13dfd2.netlify.app/",
+    githubLink: "https://github.com/dev-rome/Dictionary-Web-App",
+  },
+  {
+    id: "7",
+    title: "Weather App",
+    description:
+      "Weather App: Built with Next.js, TypeScript, Tailwind CSS, and the Weatherbit API, providing users with quick and accurate weather information.",
+    image: WeatherApp,
+    url: "https://example.com",
+    githubLink: "https://github.com/dev-rome/Weather-App",
+  },
+  {
+    id: "8",
+    title: "IP Address Tracker",
+    description:
+      "IP Address Tracker is a web application built with React and Tailwind CSS that allows users to track the location of an IP address using the IP Geolocation API.",
+    image: IPAddressTracker,
+    url: "https://example.com",
+    githubLink: "https://github.com/dev-rome/IP-Address-Tracker",
+  },
+  {
+    id: "9",
+    title: "Clock App",
+    description:
+      "Clock App is a simple clock application built with React and Tailwind CSS.",
+    image: ClockApp,
+    url: "https://example.com",
+    githubLink: "https://github.com/dev-rome/Clock-App",
+  },
+];
+
+export default projectData;
