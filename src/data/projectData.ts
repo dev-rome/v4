cfa7668eef9bbaba8431d3786d4ba2b615ab2b14
dev-rome <info.devrome@gmail.com>
2025-12-09 12:55:34 -0500

    Update project assets and data

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,4 +1,6 @@
-import TimberAndHatchet from "../assets/timber&hatchet.png";
+import RecipeFinder from "../assets/recipe-finder.png";
+import WeatherApp from "../assets/weather-app.png";
+import TimberAndHatchet from "../assets/timber-and-hatchet.png";
 import WordGame from "../assets/word-game.png";
 import ToastMessage from "../assets/toast-message.png";
 import DictionaryApp from "../assets/dictionary-app.png";
@@ -7,6 +9,24 @@ import ClockApp from "../assets/clock-app.png";
 const projectData = [
   {
     id: "1",
+    title: "Recipe Finder",
+    description:
+      "A recipe finder app that allows you to search for recipes by name, ingredients, or filter prep time and cooking time.",
+    image: RecipeFinder,
+    url: "https://fm-recipe-finder.vercel.app/",
+    githubLink: "https://github.com/dev-rome/FM-Recipe-Finder",
+  },
+  {
+    id: "2",
+    title: "Weather App",
+    description:
+      "A weather app that allows you to search for weather by city name, view 7 day forecast, current weather conditions, and hourly forecast.",
+    image: WeatherApp,
+    url: "https://fm-weather-app-cyan.vercel.app/",
+    githubLink: "https://github.com/dev-rome/FM-Weather-App",
+  },
+  {
+    id: "3",
     title: "Timber and Hatchet",
     description:
       "A company leveraging Astro, React, and Tailwind CSS to hire nearshore engineers.",
@@ -15,7 +35,7 @@ const projectData = [
     githubLink: "https://github.com/dev-rome/Website-Project-v1",
   },
   {
-    id: "2",
+    id: "4",
     title: "Word Game",
     description:
       "A clone of the popular Wordle game, offering a fun and challenging word puzzle experience.",
@@ -24,7 +44,7 @@ const projectData = [
     githubLink: "https://github.com/dev-rome/project-wordle",
   },
   {
-    id: "3",
+    id: "5",
     title: "Toast Message",
     description:
       "A UI element providing brief and unobtrusive notifications to users.",
@@ -33,7 +53,7 @@ const projectData = [
     githubLink: "https://github.com/dev-rome/project-toast",
   },
   {
-    id: "4",
+    id: "6",
     title: "Dictionary Web App",
     description:
       "A web app integrating the Free Dictionary API to provide quick and reliable word definitions.",
@@ -42,7 +62,7 @@ const projectData = [
     githubLink: "https://github.com/dev-rome/Dictionary-Web-App",
   },
   {
-    id: "5",
+    id: "7",
     title: "Clock App",
     description:
       "A simple clock application built with React and Tailwind CSS.",
